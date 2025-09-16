041786abf27cbf375bad5dccc17d019ae9a40f6f
yangkaixin <alexanderykx@outlook.com>
2025-09-16 11:38:06 +0800

    fix

--- src/hubverse_setup.R
+++ src/hubverse_setup.R
@@ -7,25 +7,25 @@
 
 setwd("/Users/ykx-gznl/Desktop/预防预警算法集成/hubverse_cn/China_COVID-19_Forecast_Hub/")
 
-# library(hubAdmin)
-# validate_config(
-#   hub_path = ".",
-#   config = c("tasks", "admin"),
-#   config_path = NULL,
-#   schema_version = "from_config",
-#   branch = getOption("hubAdmin.branch", default = "main")
-# )
-# 
-# library(hubCI)
-# use_hub_github_action(name = "validate-submission")
-# 
-# library(hubValidations)
-# v <- hubValidations::validate_pr(
-#   gh_repo = Sys.getenv("GITHUB_REPOSITORY"),
-#   pr_number = Sys.getenv("PR_NUMBER"),
-#   skip_submit_window_check = FALSE
-# )
-# hubValidations::check_for_errors(v, verbose = TRUE)
+library(hubAdmin)
+validate_config(
+  hub_path = ".",
+  config = c("tasks", "admin"),
+  config_path = NULL,
+  schema_version = "from_config",
+  branch = getOption("hubAdmin.branch", default = "main")
+)
+
+library(hubCI)
+use_hub_github_action(name = "validate-submission")
+
+library(hubValidations)
+v <- hubValidations::validate_pr(
+  gh_repo = Sys.getenv("GITHUB_REPOSITORY"),
+  pr_number = Sys.getenv("PR_NUMBER"),
+  skip_submit_window_check = FALSE
+)
+hubValidations::check_for_errors(v, verbose = TRUE)
 
 # library(hubData)
 # library(dplyr)
