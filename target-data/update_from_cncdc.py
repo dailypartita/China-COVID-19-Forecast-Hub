e0f7096ac8de55b3d2c312735fd0bca187bb61f0
dailypartita <alexanderykx@outlook.com>
2026-09-24 10:56:46 +0800

    fix(target-data): read the cncdc CSV from the crawl project under hubverse

--- target-data/update_from_cncdc.py
+++ target-data/update_from_cncdc.py
@@ -16,7 +16,7 @@ CNCDC_URL = (
     "data/cncdc_surveillance_covid19.csv"
 )
 DEFAULT_LOCAL_CSV = Path(
-    "/data/ykx/covid19/get_data/cn_cdc_data/data/cncdc_surveillance_covid19.csv"
+    "/data/ykx/hubverse_proj/cn_cdc_crawl/data/cncdc_surveillance_covid19.csv"
 )
 TARGET = "wk inc covid prop ili"
 LOCATION = "CN"
